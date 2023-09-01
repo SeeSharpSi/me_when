ca658400bcc92ed139350b2b70df8936e2089259
stompkins9 <silas.tompkins@gtri.gatech.edu>
2023-09-01 11:48:37 -0400

    added jumble flag

--- main.go
+++ main.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"fmt"
-	"os"
+    "flag"
 )
 
 func main() {
-    args := os.Args[1:]
-    lol(args)
+    jumble_flag := flag.Bool("j", false, "jubmle the output")
+    flag.Parse()
+    args := flag.Args()
+    lol(args, *jumble_flag)
 }
 
-func lol(me_when []string) {
+func lol(me_when []string, jmbl bool) {
     for _, v := range me_when {
+        if jmbl {
+            v = jumble(v)
+        }
         fmt.Printf("me when %s\n", v)
     }
+}
+
+func jumble(input string) (output string) {
+    bytes := []byte(input)
+    backwards_bytes := make([]byte, 0)
+    for _, v := range bytes {
+        backwards_bytes = append([]byte{v}, backwards_bytes...)
+    }
+    output = string(backwards_bytes)
+    return
 }
